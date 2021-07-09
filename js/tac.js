3794a5af1e85b2859d594930ab63bbbdee41302b
roweeena <roe0320@gmail.com>
2021-07-09 12:09:01 +1000

    nth commit / bug fix and easter egg

--- js/tac.js
+++ js/tac.js
@@ -17,14 +17,14 @@ $(document).ready(function() {
         if (this.id === 'x') { //if player id is "x", play as the leaf
             playerX.css('opacity', '1');
             playerO.css('opacity', '0.5');
-            // $('.cell').removeClass('playFlower');
+            $('.cell').removeClass('playFlower');
             $('.cell').addClass('playLeaf');
             player = 1;
         } else if (this.id === 'o') { // or if play as the flower
             playerO.css('opacity', '1');
             playerX.css('opacity', '0.5');
             $('.cell').addClass('playFlower');
-            // $('.cell').removeClass('playLeaf');
+
             player = 2;
         }
         $('h3').hide();
@@ -34,7 +34,8 @@ $(document).ready(function() {
     //this is the point where I started flailing like a confused T-Rex
 
     //Mark each cell with image
-    $('.cell').on('click', function() {
+    const cellClick = function () {
+    $('.cell').click(function (){
         if (!$('.player-button').hasClass('playerClick')) {
             alert("Please select an icon before you play.");
         } else if ($('.cell').hasClass('gameOver')) {
@@ -50,7 +51,7 @@ $(document).ready(function() {
             });
             playerO.css('opacity', '1'); //toggle player button to signify turn
             playerX.css('opacity', '0.5');
-            cellMark = cellMark + 1;
+            cellMark = cellMark + 1; // keep track of clicked cell
         } else {
             $('.cell').addClass('playLeaf'); //marking each clicked cell with the flower class and appending the cell with the image
             $('#board').on('click', function(event) {
@@ -65,98 +66,92 @@ $(document).ready(function() {
 
         checkWin();
     });
+  };
     const checkWin = function() {
         //check X wins - rows
         if ($('.cell').hasClass('gameOver')) {
             alert("Press restart to start over"); //if game is over, alert to start over
-        } else if ($('#0.leaf,#1.leaf,#2.leaf').length == 3) {
-            playerXScore = playerXScore + 1; //add up score
-            $('.first-player').text("🍃's score: " + playerXScore); // log it to the div
-            $(".announce").text("🍃 has won! Press restart to play again.");
-        } else if ($('#3.leaf,#4.leaf,#5.leaf').length == 3) {
+        } else if ($('#0.leaf,#1.leaf,#2.leaf').length === 3) {
+          playerXScore = playerXScore + 1; //add up score
+          $('.first-player').text("🍃's score: " + playerXScore); // log it to the div
+          $(".announce").text("🍃 has won! Press restart to play again.");
+        } else if ($('#3.leaf,#4.leaf,#5.leaf').length === 3) {
           playerXScore = playerXScore + 1;
           $('.first-player').text("🍃's score: " + playerXScore);
           $(".announce").text("🍃 has won! Press restart to play again.");
-        } else if ($('#6.leaf,#7.leaf,#8.leaf').length == 3) {
+        } else if ($('#6.leaf,#7.leaf,#8.leaf').length === 3) {
           playerXScore = playerXScore + 1;
           $('.first-player').text("🍃's score: " + playerXScore);
           $(".announce").text("🍃 has won! Press restart to play again.");
         } // columns
-        else if ($('#0.leaf,#3.leaf,#6.leaf').length == 3) {
+        else if ($('#0.leaf,#3.leaf,#6.leaf').length === 3) {
           playerXScore = playerXScore + 1;
           $('.first-player').text("🍃's score: " + playerXScore);
           $(".announce").text("🍃 has won! Press restart to play again.");
-        } else if ($('#1.leaf,#4.leaf,#7.leaf').length == 3) {
+        } else if ($('#1.leaf,#4.leaf,#7.leaf').length === 3) {
           playerXScore = playerXScore + 1;
           $('.first-player').text("🍃's score: " + playerXScore);
           $(".announce").text("🍃 has won! Press restart to play again.");
-        } else if ($('#2.leaf,#5.leaf,#8.leaf').length == 3) {
+        } else if ($('#2.leaf,#5.leaf,#8.leaf').length === 3) {
           playerXScore = playerXScore + 1;
           $('.first-player').text("🍃's score: " + playerXScore);
           $(".announce").text("🍃 has won! Press restart to play again.");
         } // diagonally
-        else if ($('#0.leaf,#4.leaf,#8.leaf').length == 3) {
+        else if ($('#0.leaf,#4.leaf,#8.leaf').length === 3) {
           playerXScore = playerXScore + 1;
           $('.first-player').text("🍃's score: " + playerXScore);
           $(".announce").text("🍃 has won! Press restart to play again.");
-        } else if ($('#2.leaf,#4.leaf,#6.leaf').length == 3) {
+        } else if ($('#2.leaf,#4.leaf,#6.leaf').length === 3) {
           playerXScore = playerXScore + 1;
           $('.first-player').text("🍃's score: " + playerXScore);
           $(".announce").text("🍃 has won! Press restart to play again.");
         }
         // check O wins
-        else if ($('#0.flower,#1.flower,#2.flower').length == 3) {
+         if ($('#0.flower,#1.flower,#2.flower').length === 3) {
           playerOScore = playerOScore + 1;
           $('.second-player').text("🌸's score: " + playerOScore);
           $(".announce").text("🌸 has won! Press restart to play again.");
-        } else if ($('#3.flower,#4.flower,#5.flower').length == 3) {
+        } else if ($('#3.flower,#4.flower,#5.flower').length === 3) {
           playerOScore = playerOScore + 1;
           $('.second-player').text("🌸's score: " + playerOScore);
           $(".announce").text("🌸 has won! Press restart to play again.");
-        } else if ($('#6.flower,#7.flower,#8.flower').length == 3) {
+        } else if ($('#6.flower,#7.flower,#8.flower').length === 3) {
           playerOScore = playerOScore + 1;
           $('.second-player').text("🌸's score: " + playerOScore);
           $(".announce").text("🌸 has won! Press restart to play again.");
         } // columns
-        else if ($('#0.flower,#3.flower,#6.flower').length == 3) {
+        else if ($('#0.flower,#3.flower,#6.flower').length === 3) {
           playerOScore = playerOScore + 1;
           $('.second-player').text("🌸's score: " + playerOScore);
           $(".announce").text("🌸 has won! Press restart to play again.");
-        } else if ($('#1.flower,#4.flower,#7.flower').length == 3) {
+        } else if ($('#1.flower,#4.flower,#7.flower').length === 3) {
           playerOScore = playerOScore + 1;
           $('.second-player').text("🌸's score: " + playerOScore);
           $(".announce").text("🌸 has won! Press restart to play again.");
-        } else if ($('#2.flower,#5.flower,#8.flower').length == 3) {
+        } else if ($('#2.flower,#5.flower,#8.flower').length === 3) {
           playerOScore = playerOScore + 1;
           $('.second-player').text("🌸's score: " + playerOScore);
           $(".announce").text("🌸 has won! Press restart to play again.");
         } // diagonally
-        else if ($('#0.flower,#4.flower,#8.flower').length == 3) {
+        else if ($('#0.flower,#4.flower,#8.flower').length === 3) {
           playerOScore = playerOScore + 1;
           $('.second-player').text("🌸's score: " + playerOScore);
           $(".announce").text("🌸 has won! Press restart to play again.");
-        } else if ($('#2.flower,#4.flower,#6.flower').length == 3) {
+        } else if ($('#2.flower,#4.flower,#6.flower').length === 3) {
           playerOScore = playerOScore + 1;
           $('.second-player').text("🌸's score: " + playerOScore);
           $(".announce").text("🌸 has won! Press restart to play again.");
         }
 
-         if (cellMark === 9) {
+         if ($(".announce").text().includes("won")) { //don't do anything
+         } else if (cellMark === 9) {
           $(".announce").text("It's a tie! Hit restart to start over.")
-          // playerOScore =  ;
-          // playerXScore =- ;
         }
           //disable board when game is over
         if ($(".announce").text().includes("won") || $(".announce").text().includes("tie")) {
             $('.cell').off("click");
             $('#board').off("click");
             $('.cell').addClass('gameOver');
-
-        }
-
-        if (playerXScore === 3 || playerOScore === 3) {
-            $('#audio').attr('src', 'css/img/Omae Wa Mou.mp3', 'volume', 0.4, 'autoplay');
-
         }
     };
     // ******** RESTART GAME *********//
@@ -172,17 +167,11 @@ $(document).ready(function() {
         $('.cell').removeClass('flower');
         $('.announce').empty();
         $('h3').show();
-        // $('.cell').on("click");
-        // $('#board').on("click");
-        // $('.cell').click();
-       //
-        if (player = 1) {
-          $('.cell').addClass('playLeaf');
-          }
+        $('.cell').on("click");
+        $('#board').on("click");
+        cellClick();
 
-       else if (player = 2) {
-         $('.cell').addClass('playFlower');
+     });
 
-         }
-        });
+cellClick();
 });
